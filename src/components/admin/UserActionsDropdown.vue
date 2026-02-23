23b3ae14115bf52e9c742486188d96efc0ffc6a4
lgt <lgault@chapsvision.com>
2026-02-23 16:31:34 +0100

    chore(routing): unplugin vue router

--- src/components/admin/UserActionsDropdown.vue
+++ src/components/admin/UserActionsDropdown.vue
@@ -10,7 +10,9 @@
         @click="handleChangeOrganization(close)"
       >
         <Icon icon="fa-building" class="text-secondary" />
-        <span class="flex-1 text-left">{{ t('admin.userActions.changeOrganization', 'Change Organization') }}</span>
+        <span class="flex-1 text-left">{{
+          t('admin.userActions.changeOrganization', 'Change Organization')
+        }}</span>
       </button>
 
       <button
@@ -18,7 +20,9 @@
         @click="handleManagePermissions(close)"
       >
         <Icon icon="fa-shield" class="text-secondary" />
-        <span class="flex-1 text-left">{{ t('admin.userActions.managePermissions', 'Manage Permissions') }}</span>
+        <span class="flex-1 text-left">{{
+          t('admin.userActions.managePermissions', 'Manage Permissions')
+        }}</span>
       </button>
 
       <div class="border-primary-stroke my-1 border-t"></div>
@@ -39,7 +43,9 @@
         @click="handleDisableUser(close)"
       >
         <Icon icon="fa-user-slash" class="text-warning" />
-        <span class="flex-1 text-left">{{ t('admin.userActions.disableUser', 'Disable User') }}</span>
+        <span class="flex-1 text-left">{{
+          t('admin.userActions.disableUser', 'Disable User')
+        }}</span>
       </button>
 
       <button
@@ -47,7 +53,9 @@
         @click="handleResetPassword(close)"
       >
         <Icon icon="fa-key" class="text-info" />
-        <span class="flex-1 text-left">{{ t('admin.userActions.resetPassword', 'Reset Password') }}</span>
+        <span class="flex-1 text-left">{{
+          t('admin.userActions.resetPassword', 'Reset Password')
+        }}</span>
       </button>
     </template>
   </Dropdown>
